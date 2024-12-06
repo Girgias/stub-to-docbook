6fa6c12cc9e3ce558c2f92da310d73475be8a82c
Gina Peter Banyard <girgias@php.net>
2024-12-06 01:13:25 +0000

    Add DocumentedFunction and parsing for <methodsynopsis>

---
+++ src/Documentation/Functions/DocumentedFunction.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Girgias\StubToDocbook\Documentation\Functions;
+
+use Girgias\StubToDocbook\Documentation\DocumentedAttribute;
+use Girgias\StubToDocbook\FP\Equatable;
+use Girgias\StubToDocbook\FP\Utils;
+use Girgias\StubToDocbook\Types\DocumentedTypeParser;
+use Girgias\StubToDocbook\Types\Type;
+
+final readonly class DocumentedFunction implements Equatable
+{
+    public function __construct(
+        readonly string $name,
+        /** @param list<DocumentedParameter> $parameters */
+        readonly array $parameters,
+        readonly Type $returnType,
+        readonly bool $byRefReturn = false,
+        /** @param array<string, DocumentedAttribute> $attributes */
+        readonly array $attributes = [],
+        readonly bool $isStatic = false,
+        readonly bool $isDeprecated = false,
+    ) {}
+
+    /**
+     * @param DocumentedFunction $other
+     */
+    public function isSame(mixed $other): bool
+    {
+        return $this->name === $other->name
+            && $this->returnType->isSame($other->returnType)
+            && $this->byRefReturn === $other->byRefReturn
+            && Utils::equateList($this->parameters, $other->parameters)
+            && Utils::equateList($this->attributes, $other->attributes)
+            && $this->isStatic === $other->isStatic
+            && $this->isDeprecated === $other->isDeprecated
+        ;
+    }
+
+    /**
+     * DocBook 5.2 <methodsynopsis> documentation
+     * URL: https://tdg.docbook.org/tdg/5.2/methodsynopsis
+     */
+    public static function parseFromDoc(\DOMElement $element): DocumentedFunction
+    {
+
+        if ($element->tagName !== 'methodsynopsis') {
+            throw new \Exception('Unexpected tag "' . $element->tagName . '"');
+        }
+
+        $name = null;
+        $returnType = null;
+        $byRefReturn = false;
+        $isStatic = false;
+        $isDeprecated = false;
+        $parameters = [];
+        $attributes = [];
+
+        foreach ($element->childNodes as $node) {
+            if ($node instanceof \DOMText) {
+                continue;
+            }
+            match ($node->tagName) {
+                'type' => $returnType = DocumentedTypeParser::parse($node),
+                'modifier'  => $attributes[] = DocumentedAttribute::parseFromDoc($node),
+                'methodname' => $name = $node->textContent,
+                'methodparam' => $parameters[] = DocumentedParameter::parseFromDoc($node, count($parameters)+1),
+                'void' => $parameters = [],
+            };
+        }
+
+        return new DocumentedFunction(
+            $name,
+            $parameters,
+            $returnType,
+            $byRefReturn,
+            $attributes,
+            $isStatic,
+            $isDeprecated,
+        );
+    }
+}
---
+++ tests/Documentation/Functions/DocumentedFunctionTest.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace Documentation\Functions;
+
+use Girgias\StubToDocbook\Documentation\DocumentedAttribute;
+use Girgias\StubToDocbook\Documentation\Functions\DocumentedFunction;
+use Girgias\StubToDocbook\Documentation\Functions\DocumentedParameter;
+use Girgias\StubToDocbook\Types\SingleType;
+use Girgias\StubToDocbook\Types\UnionType;
+use PHPUnit\Framework\TestCase;
+
+class DocumentedFunctionTest extends TestCase
+{
+
+    public function test_no_param_function_parsing(): void
+    {
+        $xml = <<<'XML'
+<methodsynopsis>
+ <type>string</type><methodname>test_function</methodname>
+ <void/>
+</methodsynopsis>
+XML;
+        $document = new \DOMDocument();
+        $document->loadXML($xml);
+        $fn = DocumentedFunction::parseFromDoc($document->firstChild);
+
+        $expectedFunction = new DocumentedFunction(
+            'test_function',
+            [],
+            new SingleType('string'),
+        );
+
+        self::assertTrue($fn->isSame($expectedFunction));
+    }
+
+    public function test_variadic_function_parsing(): void
+    {
+        $xml = <<<'XML'
+<methodsynopsis>
+ <type>bool</type><methodname>test_variadic</methodname>
+ <methodparam><type>mixed</type><parameter>var</parameter></methodparam>
+ <methodparam rep="repeat"><type>mixed</type><parameter>vars</parameter></methodparam>
+</methodsynopsis>
+XML;
+        $document = new \DOMDocument();
+        $document->loadXML($xml);
+        $fn = DocumentedFunction::parseFromDoc($document->firstChild);
+
+        $expectedFunction = new DocumentedFunction(
+            'test_variadic',
+            [
+                new DocumentedParameter(
+                    'var',
+                    1,
+                    new SingleType('mixed'),
+                ),
+                new DocumentedParameter(
+                    'vars',
+                    2,
+                    new SingleType('mixed'),
+                    isVariadic: true,
+                ),
+            ],
+            new SingleType('bool'),
+        );
+
+        self::assertTrue($fn->isSame($expectedFunction));
+    }
+
+    public function test_function_parsing_with_attribute(): void
+    {
+        $xml = <<<'XML'
+<methodsynopsis>
+ <modifier role="attribute">#[\Deprecated]</modifier>
+ <type>bool</type><methodname>test_attribute</methodname>
+ <methodparam><type>mixed</type><parameter>param1</parameter></methodparam>
+</methodsynopsis>
+XML;
+        $document = new \DOMDocument();
+        $document->loadXML($xml);
+        $fn = DocumentedFunction::parseFromDoc($document->firstChild);
+
+        $expectedFunction = new DocumentedFunction(
+            'test_attribute',
+            [
+                new DocumentedParameter(
+                    'param1',
+                    1,
+                    new SingleType('mixed'),
+                ),
+            ],
+            new SingleType('bool'),
+            attributes: [
+                new DocumentedAttribute('\\Deprecated'),
+            ]
+        );
+
+        self::assertTrue($fn->isSame($expectedFunction));
+    }
+
+    public function test_complete_function_parsing(): void
+    {
+        $xml = <<<'XML'
+<methodsynopsis>
+ <modifier role="attribute">#[\Deprecated]</modifier>
+ <type class="union"><type>string</type><type>false</type></type><methodname>test_complete_function</methodname>
+ <methodparam><type>string</type><parameter>param_typical</parameter></methodparam>
+ <methodparam><type>array</type><parameter role="reference">param_reference</parameter></methodparam>
+ <methodparam><modifier role="attribute">#[\SensitiveParameter]</modifier><type>string</type><parameter>param_sensitive</parameter></methodparam>
+ <methodparam choice="opt"><type class="union"><type>int</type><type>null</type></type><parameter>param_optional</parameter><initializer><constant>SOME_CONST</constant></initializer></methodparam>
+</methodsynopsis>
+XML;
+        $document = new \DOMDocument();
+        $document->loadXML($xml);
+        $fn = DocumentedFunction::parseFromDoc($document->firstChild);
+
+        $expectedFunction = new DocumentedFunction(
+            'test_complete_function',
+            [
+                new DocumentedParameter(
+                    'param_typical',
+                    1,
+                    new SingleType('string'),
+                ),
+                new DocumentedParameter(
+                    'param_reference',
+                    2,
+                    new SingleType('array'),
+                    isByRef: true,
+                ),
+                new DocumentedParameter(
+                    'param_sensitive',
+                    3,
+                    new SingleType('string'),
+                    attributes: [
+                        new DocumentedAttribute('\\SensitiveParameter'),
+                    ],
+                ),
+                new DocumentedParameter(
+                    'param_optional',
+                    4,
+                    new UnionType([
+                        new SingleType('int'),
+                        new SingleType('null'),
+                    ]),
+                    isOptional: true,
+                    defaultValue: 'SOME_CONST',
+                ),
+            ],
+            new UnionType([
+                new SingleType('string'),
+                new SingleType('false'),
+            ]),
+            attributes: [
+                new DocumentedAttribute('\\Deprecated'),
+            ]
+        );
+
+        self::assertTrue($fn->isSame($expectedFunction));
+    }
+}
