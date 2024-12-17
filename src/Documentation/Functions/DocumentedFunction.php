f643d3442f6b5379a907c9a70325bb6c5eaed9ed
Gina Peter Banyard <girgias@php.net>
2024-12-17 18:54:16 +0000

    Add support for parsing a function <refentry>

---
+++ src/Documentation/Functions/DocumentedFunction.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Girgias\StubToDocbook\Documentation\Functions;
+
+use Dom\Element;
+use Dom\NodeList;
+use Dom\XPath;
+
+final class DocumentedFunction
+{
+    /**
+     * @param string $id
+     * @param FunctionMetaData $functionMetaData
+     * @param list<ParameterMetaData> $documentedParameters
+     * @param list<string> $parameterTagsValues
+     */
+    public function __construct(
+        private(set) string $id,
+        private(set) FunctionMetaData $functionMetaData,
+        private(set) array $documentedParameters,
+        private(set) array $parameterTagsValues,
+    ) {}
+
+    public function areAllParametersDocumented(): bool
+    {
+        if (count($this->documentedParameters) !== count($this->functionMetaData->parameters)) {
+            return false;
+        }
+        $areEqual = array_map(
+            fn (ParameterMetaData $synopsis, ParameterMetaData $listEntry) => $synopsis->name === $listEntry->name,
+            $this->functionMetaData->parameters,
+            $this->documentedParameters,
+        );
+
+        return array_reduce(
+            $areEqual,
+            fn (bool $carry, bool $item) => $carry && $item,
+            true
+        );
+    }
+
+    public function areAllParameterTagsReferencingFunctionParameters(): bool
+    {
+        $validParamNames = array_map(
+            fn (ParameterMetaData $synopsis) => $synopsis->name,
+            $this->functionMetaData->parameters,
+        );
+        return array_reduce(
+            $this->parameterTagsValues,
+            fn (bool $carry, string $item) => $carry && in_array($item, $validParamNames, true),
+            true,
+        );
+    }
+
+    public static function parseFromDoc(Element $element): ?DocumentedFunction
+    {
+        $id = $element->id;
+        $parameters = [];
+
+        $doc = $element->ownerDocument;
+        $xpath = new XPath($doc);
+        $xpath->registerNamespace('db', 'http://docbook.org/ns/docbook');
+        /** @var NodeList<Element> $methodSynopsis */
+        $methodSynopsis = $xpath->query('db:refsect1[@role="description"]//db:methodsynopsis', $element);
+        if ($methodSynopsis->length === 0) {
+            throw new \Exception('No <methodsynopsis> tag found for function ' . $id);
+        }
+        if ($methodSynopsis->length > 1) {
+            // TODO Handle more than 1 <methodsynopsis> tag
+            return null;
+        }
+        $fn = FunctionMetaData::parseFromDoc($methodSynopsis[0]);
+
+        /* We only want to select the <varlistentry> from the top level <variablelist>.
+         * However, dumb XML markup means that we might encounter a para tag */
+        /** @var NodeList<Element> $varListEntries */
+        $varListEntries = $xpath->query('db:refsect1[@role="parameters"]//db:variablelist/db:varlistentry', $element);
+        $paramNum = 1;
+        foreach ($varListEntries as $parameter) {
+            $parameters[] = ParameterMetaData::parseFromVaListEntryDocTag($parameter, $paramNum);
+            ++$paramNum;
+        }
+
+        /** @var NodeList<Element> $parameterTags */
+        $parameterTags = $xpath->query('db:parameter', $element);
+        $parameterTags = array_map(
+            fn (Element $paramTag) => $paramTag->textContent,
+            iterator_to_array($parameterTags, false),
+        );
+
+        return new self($id, $fn, $parameters, $parameterTags);
+    }
+}
---
+++ tests/Documentation/Functions/DocumentedFunctionTest.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace Documentation\Functions;
+
+use Dom\Element;
+use Dom\XMLDocument;
+use Girgias\StubToDocbook\Documentation\AttributeMetaData;
+use Girgias\StubToDocbook\Documentation\Functions\DocumentedFunction;
+use Girgias\StubToDocbook\Documentation\Functions\FunctionMetaData;
+use Girgias\StubToDocbook\Documentation\Functions\ParameterMetaData;
+use Girgias\StubToDocbook\Types\SingleType;
+use Girgias\StubToDocbook\Types\UnionType;
+use PHPUnit\Framework\TestCase;
+
+class DocumentedFunctionTest extends TestCase
+{
+    private const GMP_INIT = __DIR__ . '/xml/gmp-init.xml';
+    private const ISSET = __DIR__ . '/xml/isset.xml';
+    private const PASSWORD_HASH = __DIR__ . '/xml/password-hash.xml';
+    public function loadXml(string $file): Element {
+        $str = file_get_contents($file);
+        self::assertIsString($str);
+        $str = str_replace('&', '&amp;', $str);
+        $xml = XMLDocument::createFromString($str);
+        $rootElement = $xml->firstElementChild;
+        self::assertInstanceOf(Element::class, $rootElement);
+        return $rootElement;
+    }
+
+    public function test_parsing_gmp_xml(): void
+    {
+        $root = $this->loadXml(self::GMP_INIT);
+        $documentedFunction = DocumentedFunction::parseFromDoc($root);
+
+        $expectedFn = new FunctionMetaData(
+            'gmp_init',
+            [
+                new ParameterMetaData(
+                    'num',
+                    1,
+                    new UnionType([
+                        new SingleType('int'),
+                        new SingleType('string'),
+                    ])
+                ),
+                new ParameterMetaData(
+                    'base',
+                    2,
+                    new SingleType('int'),
+                    isOptional: true,
+                    defaultValue: '0',
+                ),
+            ],
+            new SingleType('GMP'),
+        );
+
+        $expectedDocumentedParams = [
+            new ParameterMetaData('num', 1, new SingleType('UNKNOWN')),
+            new ParameterMetaData('base', 2, new SingleType('UNKNOWN')),
+        ];
+
+        self::assertTrue($expectedFn->isSame($documentedFunction->functionMetaData));
+        self::assertTrue($documentedFunction->areAllParametersDocumented());
+        self::assertTrue($expectedDocumentedParams[0]->isSame($documentedFunction->documentedParameters[0]));
+        self::assertTrue($expectedDocumentedParams[1]->isSame($documentedFunction->documentedParameters[1]));
+        self::assertTrue($documentedFunction->areAllParameterTagsReferencingFunctionParameters());
+    }
+
+    public function test_parsing_isset_xml(): void
+    {
+        $root = $this->loadXml(self::ISSET);
+        $documentedFunction = DocumentedFunction::parseFromDoc($root);
+
+        $expectedFn = new FunctionMetaData(
+            'isset',
+            [
+                new ParameterMetaData(
+                    'var',
+                    1,
+                    new SingleType('mixed'),
+                ),
+                new ParameterMetaData(
+                    'vars',
+                    2,
+                    new SingleType('mixed'),
+                    isVariadic: true,
+                ),
+            ],
+            new SingleType('bool'),
+        );
+
+        $expectedDocumentedParams = [
+            new ParameterMetaData('var', 1, new SingleType('UNKNOWN')),
+            new ParameterMetaData('vars', 2, new SingleType('UNKNOWN')),
+        ];
+
+        self::assertTrue($expectedFn->isSame($documentedFunction->functionMetaData));
+        self::assertTrue($documentedFunction->areAllParametersDocumented());
+        self::assertTrue($expectedDocumentedParams[0]->isSame($documentedFunction->documentedParameters[0]));
+        self::assertTrue($expectedDocumentedParams[1]->isSame($documentedFunction->documentedParameters[1]));
+        self::assertTrue($documentedFunction->areAllParameterTagsReferencingFunctionParameters());
+    }
+
+    public function test_parsing_password_hash_xml(): void
+    {
+        $root = $this->loadXml(self::PASSWORD_HASH);
+        $documentedFunction = DocumentedFunction::parseFromDoc($root);
+
+        $expectedFn = new FunctionMetaData(
+            'password_hash',
+            [
+                new ParameterMetaData(
+                    'password',
+                    1,
+                    new SingleType('string'),
+                    attributes: [
+                        new AttributeMetaData('\SensitiveParameter')
+                    ]
+                ),
+                new ParameterMetaData(
+                    'algo',
+                    2,
+                    new UnionType([
+                        new SingleType('string'),
+                        new SingleType('int'),
+                        new SingleType('null'),
+                    ])
+                ),
+                new ParameterMetaData(
+                    'options',
+                    3,
+                    new SingleType('array'),
+                    isOptional: true,
+                    defaultValue: '[]',
+                ),
+            ],
+            new SingleType('string'),
+        );
+
+        $expectedDocumentedParams = [
+            new ParameterMetaData('password', 1, new SingleType('UNKNOWN')),
+            new ParameterMetaData('algo', 2, new SingleType('UNKNOWN')),
+            new ParameterMetaData('options', 2, new SingleType('UNKNOWN')),
+        ];
+
+        self::assertTrue($expectedFn->isSame($documentedFunction->functionMetaData));
+        self::assertTrue($documentedFunction->areAllParametersDocumented());
+        self::assertTrue($expectedDocumentedParams[0]->isSame($documentedFunction->documentedParameters[0]));
+        self::assertTrue($expectedDocumentedParams[1]->isSame($documentedFunction->documentedParameters[1]));
+        self::assertTrue($documentedFunction->areAllParameterTagsReferencingFunctionParameters());
+    }
+}
