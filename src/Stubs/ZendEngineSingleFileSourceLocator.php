f23dbb80956234acd3e4ad8fbe91fe93adf8b744
Gina Peter Banyard <girgias@php.net>
2025-02-14 12:29:17 +0000

    Rewrite ZendEngineSingleFileSourceLocator to satisfy PHPStan

--- src/Stubs/ZendEngineSingleFileSourceLocator.php
+++ src/Stubs/ZendEngineSingleFileSourceLocator.php
@@ -18,20 +18,28 @@ class ZendEngineSingleFileSourceLocator extends SingleFileSourceLocator
      *
      * @throws InvalidFileLocation
      */
-    public function __construct(private string $fileName, Locator $astLocator)
+    public function __construct(private readonly string $fileName, Locator $astLocator)
     {
         parent::__construct($fileName, $astLocator);
     }
 
     protected function createLocatedSource(Identifier $identifier): LocatedSource|null
     {
+        $content = file_get_contents($this->fileName);
+        if ($content === false || strlen($content) === 0) {
+            throw new \Exception('Cannot load content of "' . $this->fileName . '"');
+        }
         return new InternalLocatedSource(
-            file_get_contents($this->fileName),
+            $content,
             $identifier->getName(),
             self::getExtensionNameFromFileName($this->fileName),
         );
     }
 
+    /**
+     * @param non-empty-string $fileName
+     * @return non-empty-string
+     */
     private static function getExtensionNameFromFileName(string $fileName): string
     {
         if (str_contains($fileName, 'Zend/')) {
@@ -41,6 +49,7 @@ class ZendEngineSingleFileSourceLocator extends SingleFileSourceLocator
         } else if (str_contains($fileName, 'ext/')) {
             $start = strpos($fileName, 'ext/') + strlen('ext/');
             $end = strpos($fileName, '/', $start);
+            /** @var non-empty-string */
             return ucfirst(substr($fileName, $start, $end - $start));
         } else if (str_contains($fileName, 'sapi/')) {
             $start = strpos($fileName, 'sapi/') + strlen('sapi/');
