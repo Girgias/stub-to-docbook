24d2c2ccce5f8f0ea62beb3aa6e816b98710ffc5
Gina Peter Banyard <girgias@php.net>
2025-08-17 16:34:05 +0200

    Add way to convert ReflectionTypes to our internal types

--- src/Types/ReflectionTypeParser.php
+++ src/Types/ReflectionTypeParser.php
@@ -3,6 +3,9 @@
 namespace Girgias\StubToDocbook\Types;
 
 use Roave\BetterReflection\Reflection\ReflectionConstant;
+use Roave\BetterReflection\Reflection\ReflectionIntersectionType;
+use Roave\BetterReflection\Reflection\ReflectionNamedType;
+use Roave\BetterReflection\Reflection\ReflectionUnionType;
 
 final class ReflectionTypeParser
 {
@@ -27,5 +30,39 @@ final class ReflectionTypeParser
     public static function parseTypeForConstant(ReflectionConstant $reflectionData): SingleType
     {
         return new SingleType(self::parseTypeForConstantAsString($reflectionData));
+    }
+
+    public static function convertFromReflectionType(
+        ReflectionNamedType|ReflectionUnionType|ReflectionIntersectionType $reflectionType
+    ): Type
+    {
+        if ($reflectionType instanceof ReflectionNamedType) {
+            $type = $reflectionType->getName();
+            if (
+                $reflectionType->allowsNull()
+                && $type[0] === '?'
+            ) {
+                $type = ltrim($type, '?');
+                return new UnionType([
+                    new SingleType($type),
+                    new SingleType('null'),
+                ]);
+            }
+            return new SingleType($type);
+        }
+        return self::fromReflectionTypeList($reflectionType);
+    }
+
+    private static function fromReflectionTypeList(
+        ReflectionUnionType|ReflectionIntersectionType $type
+    ): UnionType|IntersectionType {
+        /** @var non-empty-list<IntersectionType|SingleType> $types */
+        $types = array_map(self::convertFromReflectionType(...), $type->getTypes());
+        if ($type instanceof ReflectionUnionType) {
+            return new UnionType($types);
+        } else {
+            /** @var non-empty-list<SingleType> $types */
+            return new IntersectionType($types);
+        }
     }
 }
---
+++ tests/Types/ReflectionTypeParserTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Types;
+
+use Girgias\StubToDocbook\Stubs\ZendEngineReflector;
+use Girgias\StubToDocbook\Types\IntersectionType;
+use Girgias\StubToDocbook\Types\ReflectionTypeParser;
+use Girgias\StubToDocbook\Types\SingleType;
+use Girgias\StubToDocbook\Types\UnionType;
+use PHPUnit\Framework\TestCase;
+use Roave\BetterReflection\BetterReflection;
+use Roave\BetterReflection\Reflection\ReflectionFunction;
+use Roave\BetterReflection\SourceLocator\Type\StringSourceLocator;
+
+class ReflectionTypeParserTest extends TestCase
+{
+
+    const /* string */ STUB_FILE_STR = <<<'STUB'
+<?php
+
+/** @generate-class-entries */
+
+function simple_type(): string {}
+
+function simple_nullable_type(): ?int {}
+
+function simple_union_type_with_null(): float|null {}
+
+function simple_union_type(): float|int {}
+
+function simple_intersection_type(): Traversable&Countable {}
+
+function dnf_type(): array|(Traversable&Countable) {}
+
+STUB;
+
+
+    public function test_can_retrieve_constants(): void
+    {
+        $astLocator = (new BetterReflection())->astLocator();
+        $reflector = ZendEngineReflector::newZendEngineReflector([
+            new StringSourceLocator(self::STUB_FILE_STR, $astLocator),
+        ]);
+        $returnTypes = array_map(
+            ReflectionTypeParser::convertFromReflectionType(...),
+            array_map(
+                fn(ReflectionFunction $rf) => $rf->getReturnType(),
+                $reflector->reflectAllFunctions(),
+            ),
+        );
+
+        self::assertCount(6, $returnTypes);
+
+        $expectedSimpleType = new SingleType('string');
+        self::assertTrue($expectedSimpleType->isSame($returnTypes[0]));
+
+        $expectedSimpleNullableType = new UnionType([
+            new SingleType('int'),
+            new SingleType('null'),
+        ]);
+        self::assertTrue($expectedSimpleNullableType->isSame($returnTypes[1]));
+
+        $expectedSimpleUnionTypeWithNull = new UnionType([
+            new SingleType('float'),
+            new SingleType('null'),
+        ]);
+        self::assertTrue($expectedSimpleUnionTypeWithNull->isSame($returnTypes[2]));
+
+        $expectedSimpleUnionType = new UnionType([
+            new SingleType('float'),
+            new SingleType('int'),
+        ]);
+        self::assertTrue($expectedSimpleUnionType->isSame($returnTypes[3]));
+
+        $expectedSimpleIntersectionType = new IntersectionType([
+            new SingleType('Traversable'),
+            new SingleType('Countable'),
+        ]);
+        self::assertTrue($expectedSimpleIntersectionType->isSame($returnTypes[4]));
+
+        $expectedDnfType = new UnionType([
+            new SingleType('array'),
+            new IntersectionType([
+                new SingleType('Traversable'),
+                new SingleType('Countable'),
+            ]),
+        ]);
+        self::assertTrue($expectedDnfType->isSame($returnTypes[5]));
+    }
+}
