983d28048a078063bb1ab16799fa57462612cf2b
Gina Peter Banyard <girgias@php.net>
2025-08-18 21:38:34 +0200

    Add ConstantMetaData constructor from XML

--- src/MetaData/ConstantMetaData.php
+++ src/MetaData/ConstantMetaData.php
@@ -2,7 +2,9 @@
 
 namespace Girgias\StubToDocbook\MetaData;
 
+use Dom\Element;
 use Dom\Node;
+use Girgias\StubToDocbook\Types\DocumentedTypeParser;
 use Girgias\StubToDocbook\Types\ReflectionTypeParser;
 use Girgias\StubToDocbook\Types\SingleType;
 use Roave\BetterReflection\Reflection\ReflectionConstant;
@@ -13,7 +15,7 @@ final class ConstantMetaData
         readonly string $name,
         readonly SingleType|null $type,
         readonly string $extension,
-        readonly string|null $id = null,
+        readonly string|null $id,
         readonly string|int|float|null $value = null,
         readonly Node|null $description = null,
     ) {}
@@ -32,5 +34,43 @@ final class ConstantMetaData
             'constant.' . xmlify_labels($name),
             value: $reflectionData->getValue(),
         );
+    }
+
+
+    /**
+     * DocBook 5.2 <varlistentry> documentation
+     * URL: https://tdg.docbook.org/tdg/5.2/varlistentry
+     * Returns null if no <constant> tag exist within the <term> tag
+     */
+    public static function parseFromVarListEntryTag(Element $entry, string $extension): ?self
+    {
+        $id = null;
+        $type = null;
+        if ($entry->hasAttribute('xml:id')) {
+            $id = $entry->getAttribute('xml:id');
+        }
+
+        $terms = $entry->getElementsByTagName("term");
+        assert(count($terms) === 1);
+
+        $manualConstantTags = $terms[0]->getElementsByTagName("constant");
+        /* See reference/filter/constants.xml with Available options variable lists */
+        if ($manualConstantTags->length === 0) {
+            return null;
+        }
+        assert(count($manualConstantTags) === 1);
+        $manualConstantName = $manualConstantTags[0]->textContent;
+
+        $manualTypeTags = $terms[0]->getElementsByTagName("type");
+        if (count($manualTypeTags) === 1) {
+            $type = DocumentedTypeParser::parse($manualTypeTags[0]);
+            assert($type instanceof SingleType);
+        }
+
+        $manualListItemTags = $entry->getElementsByTagName("listitem");
+        /* Guaranteed by the DocBook schema */
+        assert(count($manualListItemTags) === 1);
+        $manualListItem = $manualListItemTags[0];
+        return new self($manualConstantName, $type, $extension, $id, description: $manualListItem);
     }
 }
--- tests/MetaData/ConstantMetaDataTest.php
+++ tests/MetaData/ConstantMetaDataTest.php
@@ -2,6 +2,7 @@
 
 namespace MetaData;
 
+use Dom\XMLDocument;
 use Girgias\StubToDocbook\MetaData\ConstantMetaData;
 use Girgias\StubToDocbook\Stubs\StubConstantList;
 use Girgias\StubToDocbook\Stubs\ZendEngineReflector;
@@ -63,5 +64,80 @@ STUB;
     {
         self::assertSame($name, $constant->name);
         self::assertTrue($type->isSame($constant->type));
+    }
+
+    public function test_varlistentry_constant_parsing_all_data(): void
+    {
+        $xml = <<<'XML'
+<varlistentry xml:id="constant.stdout">
+ <term>
+  <constant>STDOUT</constant>
+  (<type>resource</type>)
+ </term>
+ <listitem>
+  <simpara>
+   An already opened stream to <literal>stdout</literal>.
+   Available only under the CLI SAPI.
+  </simpara>
+ </listitem>
+</varlistentry>
+XML;
+        $document = XMLDocument::createFromString($xml);
+        $constant = ConstantMetaData::parseFromVarListEntryTag($document->firstElementChild, 'UNKNOWN');
+
+        $expectedType = new SingleType('resource');
+
+        self::assertSame('constant.stdout', $constant->id);
+        self::assertSame('STDOUT', $constant->name);
+        self::assertTrue($expectedType->isSame($constant->type));
+    }
+
+    public function test_varlistentry_constant_parsing_missing_type(): void
+    {
+        $xml = <<<'XML'
+<varlistentry xml:id="constant.stdout">
+ <term>
+  <constant>STDOUT</constant>
+ </term>
+ <listitem>
+  <simpara>
+   An already opened stream to <literal>stdout</literal>.
+   Available only under the CLI SAPI.
+  </simpara>
+ </listitem>
+</varlistentry>
+XML;
+        $document = XMLDocument::createFromString($xml);
+        $constant = ConstantMetaData::parseFromVarListEntryTag($document->firstElementChild, 'UNKNOWN');
+
+        self::assertSame('constant.stdout', $constant->id);
+        self::assertSame('STDOUT', $constant->name);
+        self::assertNull($constant->type);
+    }
+
+    public function test_varlistentry_constant_parsing_missing_linkage_id(): void
+    {
+        $xml = <<<'XML'
+<varlistentry>
+ <term>
+  <constant>NAME</constant>
+  (<type>T</type>)
+ </term>
+ <listitem>
+  <simpara>
+   An already opened stream to <literal>stdout</literal>.
+   Available only under the CLI SAPI.
+  </simpara>
+ </listitem>
+</varlistentry>
+XML;
+        $document = XMLDocument::createFromString($xml);
+        $constant = ConstantMetaData::parseFromVarListEntryTag($document->firstElementChild, 'UNKNOWN');
+
+        $expectedType = new SingleType('T');
+
+        self::assertNull($constant->id);
+        self::assertSame('NAME', $constant->name);
+        self::assertTrue($expectedType->isSame($constant->type));
     }
 }
