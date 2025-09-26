8c16ab76c40d80256d63a3c196c2507a1b9a99a5
Gina Peter Banyard <girgias@php.net>
2025-09-26 19:21:59 +0100

    Use 8.5 as master

--- scripts/migration-diff.php
+++ scripts/migration-diff.php
@@ -7,7 +7,7 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 require_once 'stub-utils.php';
 
 $prior_version_dir = dirname(__DIR__, 2) . '/PHP-8.4/';
-$master_dir = dirname(__DIR__, 2) . '/php-src/';
+$master_dir = dirname(__DIR__, 2) . '/PHP-8.5/';
 
 $prior_version_reflector = get_reflector($prior_version_dir);
 $prior_version_constants = ConstantList::fromReflectionDataArray($prior_version_reflector->reflectAllConstants(), IGNORED_CONSTANTS);
@@ -22,7 +22,7 @@ echo 'There are currently:', PHP_EOL,
     count($diff->removed), ' constants have been removed', PHP_EOL;
 
 echo "Total 8.4 constants parsed = ", count($prior_version_constants), "\n";
-echo "Total master constants parsed = ", count($master_constants), "\n";
+echo "Total 8.5 constants parsed = ", count($master_constants), "\n";
 
 foreach ($diff->new->constants as $newConstant) {
     echo 'New constant ', $newConstant->name, ' from extension ', $newConstant->extension, ' with type:', "\n",
