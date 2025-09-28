0ff42f7bdbb7a6a3ddc5b8393b3ed41b2db04771
Gina Peter Banyard <girgias@php.net>
2025-09-28 11:55:10 +0100

    Implement a simple stub function differ
    This is the minimum required to be able to generate most of the migration guide.
    
    However a more complete version requires diffing the signatures of the functions

---
+++ tests/Differ/FunctionListDifferTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Differ;
+
+use Girgias\StubToDocbook\Differ\FunctionListDiffer;
+use Girgias\StubToDocbook\MetaData\Functions\FunctionMetaData;
+use Girgias\StubToDocbook\Stubs\ZendEngineReflector;
+use Girgias\StubToDocbook\Tests\ZendEngineStringSourceLocator;
+use PHPUnit\Framework\TestCase;
+use Roave\BetterReflection\BetterReflection;
+
+class FunctionListDifferTest extends TestCase
+{
+
+    const string BASE_STUB_FILE_STR = <<<'STUB'
+<?php
+
+/** @generate-class-entries */
+
+function no_change(int $a, int $b): bool {}
+
+function signature_change(int $aa): bool {}
+
+function will_be_removed(): void {}
+
+/**
+ * @return array<string, int|string>|false
+ * @refcount 1
+ */
+#[\Deprecated(since: '8.2', message: 'existing dep message')]
+function strptime(string $timestamp, string $format): array|false {}
+
+/**
+ * @param resource $stream
+ */
+function will_be_deprecated($stream, int $seconds, int $microseconds = 0): bool {}
+STUB;
+    const string NEW_STUB_FILE_STR = <<<'STUB'
+<?php
+
+/** @generate-class-entries */
+
+function no_change(int $a, int $b): bool {}
+
+function signature_change(int $aa, ?string $new_param = null): bool {}
+
+/**
+ * @return array<string, int|string>|false
+ * @refcount 1
+ */
+#[\Deprecated(since: '8.2', message: 'existing dep message')]
+function strptime(string $timestamp, string $format): array|false {}
+
+/**
+ * @param resource $stream
+ */
+#[\Deprecated(since: '8.5', message: "new deprecation message")]
+function will_be_deprecated($stream, int $seconds, int $microseconds = 0): bool {}
+
+function new_function(int $a, int $b): bool {}
+STUB;
+
+    public function test_diff_between_two_stub_files(): void
+    {
+        $astLocator = (new BetterReflection())->astLocator();
+
+        $baseReflector = ZendEngineReflector::newZendEngineReflector([
+            new ZendEngineStringSourceLocator(self::BASE_STUB_FILE_STR, $astLocator),
+        ]);
+        $baseSymbols = from_better_reflection_list_to_metadata($baseReflector->reflectAllFunctions());
+
+        $newReflector = ZendEngineReflector::newZendEngineReflector([
+            new ZendEngineStringSourceLocator(self::NEW_STUB_FILE_STR, $astLocator),
+        ]);
+        $newSymbols = from_better_reflection_list_to_metadata($newReflector->reflectAllFunctions());
+
+        $diff = FunctionListDiffer::stubDiff($baseSymbols, $newSymbols);
+        self::assertCount(1, $diff->new);
+        self::assertArrayHasKey('new_function', $diff->new);
+        self::assertSame('new_function', $diff->new['new_function']->name);
+
+        self::assertCount(1, $diff->removed);
+        self::assertArrayHasKey('will_be_removed', $diff->removed);
+        self::assertSame('will_be_removed', $diff->removed['will_be_removed']->name);
+
+        self::assertCount(1, $diff->deprecated);
+        self::assertArrayHasKey('will_be_deprecated', $diff->deprecated);
+        self::assertSame('will_be_deprecated', $diff->deprecated['will_be_deprecated']->name);
+    }
+}
