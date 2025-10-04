5d451d7c8021e5fa9a2b3ec4cb3e355ee8e7dbd6
Gina Peter Banyard <girgias@php.net>
2025-10-04 16:12:22 +0100

    Add Visibility enum

---
+++ src/MetaData/Visibility.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Girgias\StubToDocbook\MetaData;
+
+use Roave\BetterReflection\Reflection\ReflectionClassConstant;
+use Roave\BetterReflection\Reflection\ReflectionMethod;
+use Roave\BetterReflection\Reflection\ReflectionProperty;
+
+enum Visibility
+{
+    case Public;
+    case Protected;
+    case Private;
+
+    public static function fromReflectionData(ReflectionProperty|ReflectionClassConstant|ReflectionMethod $reflectionData): self
+    {
+        return $reflectionData->isPrivate() ?
+            Visibility::Private :
+            ($reflectionData->isProtected() ? Visibility::Protected : Visibility::Public);
+    }
+}
