06a3585478730414b3213bc027362daca24f40fe
lacatoire <la.catoire@gmail.com>
2026-02-16 10:25:27 +0100

    Add PropertyMetaData documentation parsing (parseFromDoc)
    Parse class properties from DocBook <fieldsynopsis> elements including
    type, visibility, readonly, static, and default values.
    
    Closes Girgias/stub-to-docbook#59

--- src/MetaData/Classes/PropertyMetaData.php
+++ src/MetaData/Classes/PropertyMetaData.php
@@ -2,9 +2,12 @@
 
 namespace Girgias\StubToDocbook\MetaData\Classes;
 
+use Dom\Element;
+use Dom\Text;
 use Girgias\StubToDocbook\MetaData\AttributeMetaData;
 use Girgias\StubToDocbook\MetaData\Initializer;
 use Girgias\StubToDocbook\MetaData\Visibility;
+use Girgias\StubToDocbook\Types\DocumentedTypeParser;
 use Girgias\StubToDocbook\Types\ReflectionTypeParser;
 use Girgias\StubToDocbook\Types\Type;
 use Roave\BetterReflection\Reflection\ReflectionProperty;
@@ -26,6 +29,56 @@ final class PropertyMetaData
         readonly bool $isDeprecated = false,
     ) {}
 
+
+    /**
+     * DocBook 5.2 <fieldsynopsis> documentation
+     * URL: https://tdg.docbook.org/tdg/5.2/fieldsynopsis
+     */
+    public static function parseFromDoc(Element $element): self
+    {
+        if ($element->tagName !== 'fieldsynopsis') {
+            throw new \Exception('Unexpected tag "' . $element->tagName . '"');
+        }
+
+        $name = null;
+        $type = null;
+        $defaultValue = null;
+        $visibility = Visibility::Public;
+        $isStatic = false;
+        $isReadOnly = false;
+
+        foreach ($element->childNodes as $node) {
+            if ($node instanceof Text) {
+                continue;
+            }
+            if (($node instanceof Element) === false) {
+                throw new \Exception("Unexpected node type: " . $node::class);
+            }
+            match ($node->tagName) {
+                'modifier' => match ($node->textContent) {
+                    'public' => $visibility = Visibility::Public,
+                    'protected' => $visibility = Visibility::Protected,
+                    'private' => $visibility = Visibility::Private,
+                    'static' => $isStatic = true,
+                    'readonly' => $isReadOnly = true,
+                    default => null,
+                },
+                'type' => $type = DocumentedTypeParser::parse($node),
+                'varname' => $name = $node->textContent,
+                'initializer' => $defaultValue = Initializer::parseFromDoc($node),
+                default => throw new \Exception('Unexpected <fieldsynopsis> child tag: <' . $node->tagName . '>'),
+            };
+        }
+
+        return new self(
+            $name,
+            $type,
+            defaultValue: $defaultValue,
+            visibility: $visibility,
+            isReadOnly: $isReadOnly,
+            isStatic: $isStatic,
+        );
+    }
 
     public static function fromReflectionData(ReflectionProperty $reflectionData): self
     {
