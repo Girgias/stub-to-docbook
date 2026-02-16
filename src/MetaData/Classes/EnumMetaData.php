87f657efb7f0c2f4715c70387865585567558d65
lacatoire <la.catoire@gmail.com>
2026-02-16 10:23:06 +0100

    Add EnumMetaData and EnumCaseMetaData for enum reflection parsing
    Parse PHP enums from stub files via BetterReflection including
    backed type, cases with values, methods, interfaces, and attributes.
    
    Closes Girgias/stub-to-docbook#54

---
+++ src/MetaData/Classes/EnumMetaData.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Girgias\StubToDocbook\MetaData\Classes;
+
+use Girgias\StubToDocbook\MetaData\AttributeMetaData;
+use Girgias\StubToDocbook\MetaData\Functions\FunctionMetaData;
+use Girgias\StubToDocbook\MetaData\Visibility;
+use Girgias\StubToDocbook\Types\ReflectionTypeParser;
+use Girgias\StubToDocbook\Types\SingleType;
+use Girgias\StubToDocbook\Types\Type;
+use Roave\BetterReflection\Reflection\ReflectionEnum;
+
+final class EnumMetaData
+{
+    /**
+     * @param list<EnumCaseMetaData> $cases
+     * @param list<FunctionMetaData> $methods
+     * @param list<string> $implements
+     * @param list<AttributeMetaData> $attributes
+     */
+    public function __construct(
+        readonly string $name,
+        readonly ?Type $backingType,
+        readonly array $cases,
+        readonly array $methods,
+        readonly string $extension,
+        readonly array $implements = [],
+        readonly array $attributes = [],
+        readonly bool $isDeprecated = false,
+    ) {}
+
+    public static function fromReflectionData(ReflectionEnum $reflectionData): self
+    {
+        $backingType = null;
+        if ($reflectionData->isBacked()) {
+            $backingType = ReflectionTypeParser::convertFromReflectionType($reflectionData->getBackingType());
+        }
+
+        $cases = array_map(
+            EnumCaseMetaData::fromReflectionData(...),
+            $reflectionData->getCases(),
+        );
+
+        $methods = array_values(array_filter(
+            array_map(
+                FunctionMetaData::fromReflectionData(...),
+                $reflectionData->getMethods(),
+            ),
+            fn(FunctionMetaData $m) => !in_array($m->name, ['cases', 'from', 'tryFrom'], true),
+        ));
+
+        $implements = array_map(
+            fn($interface) => $interface->getName(),
+            $reflectionData->getInterfaces(),
+        );
+
+        $attributes = array_map(
+            AttributeMetaData::fromReflectionData(...),
+            $reflectionData->getAttributes(),
+        );
+
+        return new self(
+            $reflectionData->getName(),
+            $backingType,
+            $cases,
+            $methods,
+            extension: $reflectionData->getExtensionName(),
+            implements: array_values($implements),
+            attributes: $attributes,
+            isDeprecated: $reflectionData->isDeprecated(),
+        );
+    }
+}
