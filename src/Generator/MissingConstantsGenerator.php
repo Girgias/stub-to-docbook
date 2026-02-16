6270efc567570f0c997d2253fb3e789777ab5bce
lacatoire <la.catoire@gmail.com>
2026-02-16 10:31:32 +0100

    Generate missing constants documentation
    Add MissingConstantsGenerator that produces DocBook XML for constants
    found in stubs but not in documentation, with extension grouping support.
    
    Closes Girgias/stub-to-docbook#37

---
+++ src/Generator/MissingConstantsGenerator.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Girgias\StubToDocbook\Generator;
+
+use Dom\XMLDocument;
+use Girgias\StubToDocbook\MetaData\Lists\ConstantList;
+
+final class MissingConstantsGenerator
+{
+    /**
+     * Generate DocBook XML string for a list of missing constants.
+     */
+    public static function generate(ConstantList $missingConstants): string
+    {
+        if (count($missingConstants) === 0) {
+            return '';
+        }
+
+        $document = XMLDocument::createEmpty();
+        $element = $missingConstants->toXml($document, 0);
+        $document->append($element);
+        $document->formatOutput = true;
+
+        return $document->saveXml($element);
+    }
+
+    /**
+     * Generate DocBook XML grouped by extension.
+     * @return array<string, string> Extension name => XML string
+     */
+    public static function generateByExtension(ConstantList $missingConstants): array
+    {
+        $byExtension = [];
+        foreach ($missingConstants->constants as $constant) {
+            $byExtension[$constant->extension][$constant->name] = $constant;
+        }
+
+        $result = [];
+        foreach ($byExtension as $extension => $constants) {
+            $list = new ConstantList($constants);
+            $result[$extension] = self::generate($list);
+        }
+
+        return $result;
+    }
+}
